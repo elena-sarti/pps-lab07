dd90edb341923f9cc19721693de3ca2354e9babf
Elena Sarti <elena.sarti8@studio.unibo.it>
2026-04-28 15:36:12 +0200

    ex3

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,7 +1,16 @@
 package ex3
 
 object Solitaire extends App:
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
+  type Position = (Int, Int)
+  def Position(i: Int, i1: Int): Position = (i, i1)
+  type Solution = List[Position]
+  def Solution(p: Position): Solution = List.apply(p)
+  type IterableFactory = Solution => Iterable[Solution]
+
+  given IterableFactory = List(_)
+  case class Board(width: Int, height: Int)
+
+  def render(solution: Solution, width: Int, height: Int): String =
     val reversed = solution.reverse
     val rows =
       for y <- 0 until height
@@ -11,5 +20,35 @@ object Solitaire extends App:
       yield row.mkString
     rows.mkString("\n")
 
+  def placeMarks(board: Board)(using factory: IterableFactory): Iterable[Solution] = (board.width, board.height) match
+    case (0, 0) => factory(List())
+    case (w, h) =>
+      val initialMark = Position(w / 2, h / 2)
+      def _placeMarks(marks: Solution): Iterable[Solution] =
+        if marks.size == w * h then factory(marks) else
+          for
+            x <- 0 until w
+            y <- 0 until h
+            mark = Position(x, y)
+            startingMark = marks.head
+            if isValid(mark, startingMark)
+            if !marks.contains(mark)
+            solution <- _placeMarks(mark +: marks)
+          yield
+            solution
+      _placeMarks(Solution(initialMark))
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  private def isValid(position: Position, initialPosition: Position): Boolean =
+    val x = position._1; val y = position._2; val initialX = initialPosition._1; val initialY = initialPosition._2
+    (math.abs(x - initialX) == 3 && y == initialY)
+      || (x == initialX && math.abs(y - initialY) == 3)
+      || (math.abs(x - initialX) == 2 && math.abs(y - initialY) == 2)
+
+  @main
+  def printSolutions(): Unit =
+    val solutions = placeMarks(Board(5, 5))
+    if (solutions.isEmpty)
+      println("No solution found")
+    else
+      solutions.foreach(s =>
+        println("Possible solution: \n" + render(s, 5, 5)))
